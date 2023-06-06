0d37042cb1ac1be76fa602b3f5b493582a3f44a1
lizexin <zexin-li@outlook.com>
2023-06-06 12:34:27 +0800

    修复返回中文时的编码问题

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
+import json
 import os
 
 import openai
 from dotenv import load_dotenv
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from langchain.llms import AzureOpenAI
 
 load_dotenv()
@@ -21,15 +23,24 @@ def hello():
     return jsonify({'message': f'Hello, {name}!'})
 
 
+# 创建返回结果为json的response，且json中出现中文时不会被编码
+def json_response(json_data):
+    response_json_str = json.dumps(json_data, ensure_ascii=False)
+    return Response(response_json_str, content_type='application/json')
+
+
 @app.route('/completion', methods=['POST'])
 def completion():
-    data = request.get_json()
-    prompt = data.get('prompt')
+    request_body = request.get_json()
+
+    prompt = request_body.get('prompt')
     if not prompt:
         return jsonify({'code': 400, 'msg': 'prompt 参数不能为空'})
+
     llm = AzureOpenAI(deployment_name=os.getenv("OPENAI_ENGINE_DEPLOYMENT_NAME"))
-    response = llm(prompt)
-    return jsonify({'code': 200, 'msg': 'success', 'data': response})
+    response_text = llm(prompt)
+
+    return json_response({'code': 200, 'msg': 'success', 'data': response_text.strip()})
 
 
 # 启动Web服务
