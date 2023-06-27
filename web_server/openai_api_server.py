2127bfbf2ac6d46fd7ff1e7524e47fa049abf55a
lizexin <zexin-li@outlook.com>
2023-06-27 11:23:39 +0800

    删除一些暂时用不上的代码

--- web_server/openai_api_server.py
+++ web_server/openai_api_server.py
@@ -1,15 +1,10 @@
 import asyncio
-import logging
 import random
-import uuid
-from datetime import datetime
+import traceback
 
 import fastapi
 import uvicorn
-from fastapi import Request, Response
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse, JSONResponse
-from starlette.middleware.base import RequestResponseEndpoint
 
 import openai_api_helper
 from openai_api_protocol import (
@@ -24,57 +19,13 @@ from openai_api_protocol import (
     ErrorCode
 )
 
-logging.basicConfig(datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO, encoding="utf-8")
-console_handler = logging.StreamHandler()
-logging.getLogger().addHandler(console_handler)
-
-logger = logging.getLogger(__name__)
 app = fastapi.FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins="*",
-    allow_methods="*",
-    allow_headers="*",
-)
 
 
 def create_error_response(code: int, message: str) -> JSONResponse:
     return JSONResponse(
         ErrorResponse(message=message, code=code).dict(), status_code=400
     )
-
-
-def now_date_time_str():
-    return datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-
-
-# @app.middleware("http")
-async def log_requests(request: Request, call_next: RequestResponseEndpoint) -> Response:
-    trace_id = str(uuid.uuid4()).replace("-", "")
-    logger.info(f"{now_date_time_str()} {trace_id} Request received: {request.method} {request.url}?{request.path_params if request.path_params else ''} {await request.body()}")
-
-    start_time = datetime.utcnow()
-    response = await call_next(request)
-    process_time = (datetime.utcnow() - start_time).total_seconds()
-
-    logger.info(f"{now_date_time_str()} {trace_id} Response sent: {response.status_code} ({process_time} seconds)")
-
-    return response
-
-
-# 来源 https://github.com/lm-sys/FastChat/blob/main/fastchat/serve/openai_api_server.py
-# https://github.com/tiangolo/fastapi/tree/master/fastapi
-@app.post("/v1/completions")
-async def create_completion(ai_request: CompletionRequest):
-    try:
-        if ai_request.stream:
-            generator = generate_completion_stream_generator(ai_request)
-            return StreamingResponse(generator, media_type="text/event-stream")
-        else:
-            return openai_api_helper.create_completion(ai_request)
-    except Exception as e:
-        logger.error(str(e), e)
-        return create_error_response(ErrorCode.INTERNAL_ERROR, str(e))
 
 
 async def generate_completion_stream_generator(ai_request: CompletionRequest):
@@ -103,23 +54,6 @@ async def generate_completion_stream_generator(ai_request: CompletionRequest):
     yield "data: [DONE]\n\n"
 
 
-@app.post("/v1/chat/completions")
-async def create_chat_completion(ai_request: ChatCompletionRequest):
-    try:
-        if ai_request.stream:
-            generator = generate_chat_completion_stream_generator(ai_request)
-            response = StreamingResponse(generator, media_type="text/event-stream", headers={
-                "Cache-Control": "no-cache, must-revalidate",
-                "Connection": "keep-alive"
-            })
-            return response
-        else:
-            return openai_api_helper.create_chat_completion(ai_request)
-    except Exception as e:
-        logger.error(str(e), e)
-        return create_error_response(ErrorCode.INTERNAL_ERROR, str(e))
-
-
 async def generate_chat_completion_stream_generator(ai_request: ChatCompletionRequest):
     async for chunk in openai_api_helper.create_chat_completion_stream(ai_request):
         choices = []
@@ -129,6 +63,7 @@ async def generate_chat_completion_stream_generator(ai_request: ChatCompletionRe
                 delta.role = choice['delta']['role']
             if 'content' in choice['delta']:
                 delta.content = choice['delta']['content']
+
             choices.append(
                 ChatCompletionResponseStreamChoice(
                     index=choice.index,
@@ -149,6 +84,38 @@ async def generate_chat_completion_stream_generator(ai_request: ChatCompletionRe
         await asyncio.sleep(random.uniform(0.01, 0.1))
 
     yield "data: [DONE]\n\n"
+
+
+# 来源 https://github.com/lm-sys/FastChat/blob/main/fastchat/serve/openai_api_server.py
+# https://github.com/tiangolo/fastapi/tree/master/fastapi
+@app.post("/v1/completions")
+async def create_completion(ai_request: CompletionRequest):
+    try:
+        if ai_request.stream:
+            generator = generate_completion_stream_generator(ai_request)
+            return StreamingResponse(generator, media_type="text/event-stream")
+        else:
+            return openai_api_helper.create_completion(ai_request)
+    except Exception as e:
+        traceback.print_exception(e)
+        return create_error_response(ErrorCode.INTERNAL_ERROR, str(e))
+
+
+@app.post("/v1/chat/completions")
+async def create_chat_completion(ai_request: ChatCompletionRequest):
+    try:
+        i = 1 / 0
+        if ai_request.stream:
+            response = StreamingResponse(
+                generate_chat_completion_stream_generator(ai_request),
+                media_type="text/event-stream",
+            )
+            return response
+        else:
+            return openai_api_helper.create_chat_completion(ai_request)
+    except Exception as e:
+        traceback.print_exception(e)
+        return create_error_response(ErrorCode.INTERNAL_ERROR, str(e))
 
 
 if __name__ == '__main__':
